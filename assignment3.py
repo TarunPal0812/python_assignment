fff248945bb88aad1f4dab66488e4f13c30f6944
Tarun Pal <143593638+TarunPal0812@users.noreply.github.com>
2026-09-03 11:07:59 +0530

    comp: Complete assignment 3

---
+++ assignment3.py
@@ -0,0 +1,15 @@
+def flatten_dict(d, parent_key='', sep='_'):
+    items = []
+    for k, v in d.items():
+        new_key = f"{parent_key}{sep}{k}" if parent_key else k
+        if isinstance(v, dict):
+            items.extend(flatten_dict(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
+
+
+nested = {'a': 1, 'b': {'c': 2, 'd': {'e': 3}}}
+print(flatten_dict(nested, sep='.'))
+
+# Output: {'a': 1, 'b.c': 2, 'b.d.e': 3}
